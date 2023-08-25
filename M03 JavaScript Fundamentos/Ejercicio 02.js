fa9f701b441c87f6c438fb5637e0248d785e22d1
TamaraGRodriguez <tamagrodriguez@gmail.com>
2023-08-24 21:53:40 -0300

    commit ejercicio02

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -1,37 +1,41 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
-function devolverString(string) {
-   // Debe retornar un string.
-   // Tu código:
+function devolverString(Hola) {
+   return (Hola)
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
-   // Retorna el resultado de su suma.
-   // Tu código:
+   return (x+y);
 }
+
+suma (8 + 9);
 
 function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
+   return x-y;
 }
+
+resta(29-9)
 
 function divide(x, y) {
-   // Retorna el resultado de su división.
-   // Tu código:
+   return x/y;
 }
+
+divide(30/3)
 
 function multiplica(x, y) {
-   // Retorna el resultado de su multiplicación.
-   // Tu código:
+   return(x*y)
 }
 
+multiplica(5*10)
+
 function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+   return(x%y)
 }
+
+obtenerResto(21%5)
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
